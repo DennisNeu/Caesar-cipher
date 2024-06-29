adb9ae0a6c9cc97b9573de49a328a3f2bd580fdb
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-29 18:27:50 +0200

    Add defensive clauses

---
+++ script.rb
@@ -0,0 +1,14 @@
+def caesar_cipher(string, shift_factor)
+  unless shift_factor <= 0
+    puts "Error"
+    return
+  end
+
+  unless string.length == 0
+    puts "Error"
+    return
+  end
+
+end
+
+caesar_cipher("", 2)
