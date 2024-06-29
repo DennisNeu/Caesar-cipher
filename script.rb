de77a354d20350a42877557b75fd84fffaaee562
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-06-29 18:30:22 +0200

    Fix bug in defensive clauses

--- script.rb
+++ script.rb
@@ -1,14 +1,19 @@
 def caesar_cipher(string, shift_factor)
-  unless shift_factor <= 0
+  unless shift_factor > 0
     puts "Error"
     return
   end
 
-  unless string.length == 0
+  unless string.length != 0
     puts "Error"
     return
   end
 
+  string_array = string.split("")
+
+  puts string_array
 end
 
-caesar_cipher("", 2)
+
+
+caesar_cipher("Test", 2)
